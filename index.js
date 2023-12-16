6b536faedd14ef4046c8c870ad68f95d7467770d
Tak <takeshiseodev@gmail.com>
2023-12-16 17:10:43 -0500

    update: final

--- index.js
+++ index.js
@@ -1,26 +1,47 @@
-const niceList = document.getElementById("nice-list")
-const naughtyList = document.getElementById("naughty-list")
-const btn = document.getElementById("btn")
-// btn.addEventListener("click", sort)
+/** @format */
+
+const niceList = document.getElementById('nice-list')
+const naughtyList = document.getElementById('naughty-list')
+const btn = document.getElementById('btn')
 
 const sorteesArr = [
     {
-        name: "David",
-        hasBeenGood: false
+        name: 'David',
+        hasBeenGood: false,
     },
     {
-        name: "Del",
-        hasBeenGood: true
+        name: 'Del',
+        hasBeenGood: true,
     },
     {
-        name: "Valerie",
-        hasBeenGood: false
+        name: 'Valerie',
+        hasBeenGood: false,
     },
     {
-        name: "Astrid",
-        hasBeenGood: true
-    }
+        name: 'Astrid',
+        hasBeenGood: true,
+    },
 ]
+
+btn.addEventListener('click', sortPeople)
+
+function sortPeople() {
+    const nicePeople = sorteesArr.filter(person => person.hasBeenGood)
+    const naughtyPeople = sorteesArr.filter(person => !person.hasBeenGood)
+
+    updateList(niceList, nicePeople)
+    updateList(naughtyList, naughtyPeople)
+}
+
+function updateList(listElement, people) {
+    listElement.innerHTML = '' 
+    people.forEach(person => {
+        const listItem = document.createElement('li')
+        listItem.textContent = person.name
+        listElement.appendChild(listItem)
+    })
+}
+
 
 /** Challenge: 
   - Write the JavaScript to sort the people in sorteesArr into the naughty and nice lists, according to whether they have been good or not. Then display the names in the relevant place in the DOM.
